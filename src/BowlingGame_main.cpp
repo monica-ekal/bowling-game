e87cc4a7016cc9e177f9a9f274495fdf8f4fe62b
Monica Ekal <ekalmonica36@gmail.com>
2019-08-11 16:43:49 +0100

    Update BowlingGame_main.cpp

--- src/BowlingGame_main.cpp
+++ src/BowlingGame_main.cpp
@@ -6,7 +6,7 @@
 
 int main (int argc, char *argv[]) {
     BowlingGame game;                       // Score calculation class
-    Interface console;                      // Requesting output and displaying score class
+    Interface console;                      // Class for requesting output and displaying score
     int pins;
     // Player gets 20 rolls unless a spare or strike is rolled in the last frame, when it is 21.
     while (console.getRollNumber() < 21 || (console.getRollNumber() == 21 && game.isExtraRoll())) {
