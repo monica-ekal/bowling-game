0e34a3612533f6842e2372cb3c70cd0dcabea494
m-ekl <ekalmonica36@gmail.com>
2019-08-11 16:31:14 +0100

    updated README

--- includes/Interface.cpp
+++ includes/Interface.cpp
@@ -12,14 +12,14 @@
 
 //Ensure that the number of pins is valid: The total pins knocked down in each frame can be at most 10.
 int Interface::nextRoll(const BowlingGame& game){
-    if(rollNumber%2!=0 || game.getIsStrike()) prevRoll = 0 ;
+    if (rollNumber%2!=0 || game.getIsStrike()) prevRoll = 0 ;
     int pins{};
     std::cout<<"Enter number of pins knocked down from";
     for (int i = 0; i<11;i++)
-        if(i+prevRoll<=10) std::cout<<"  "<<i;
+        if (i+prevRoll<=10) std::cout<<"  "<<i;
     std::cout<<": ";
     std::cin>>pins;
-    while(pins+prevRoll>10){
+    while (pins+prevRoll>10){
         std::cout<<"Please enter a number from the above choices: ";
         std::cin>>pins;
     }
@@ -31,7 +31,7 @@ int Interface::nextRoll(const BowlingGame& game){
 //[Through frames 1-9, a strike is counted as two rolls because no more pins are left for that frame]
 void Interface::prepareNextRoll(const BowlingGame& game){
 
-    if(rollNumber>=19 && game.isExtraRoll()) {
+    if (rollNumber>=19 && game.isExtraRoll()) {
         rollNumber++;
     } else {
         game.getIsStrike() ? rollNumber += 2 : rollNumber++;
@@ -53,38 +53,38 @@ void Interface::displayScore(const BowlingGame& game) {
     // printing the number of bottles knocked down
     for (int index = 0; index<game.rolls.size(); index++)
     {
-        if(checkVector(index, game.frames)) {
+        if (checkVector(index, game.frames)) {
 
             std::cout<<" | ";
-            if(checkVector(index, game.strikes)) std::cout<<"      x";
+            if (checkVector(index, game.strikes)) std::cout<<"      x";
             else {
                 std::cout << game.rolls[index];
                 std::cout<<"    ";}
 
         } else {
 
-            if(checkVector(index, game.strikes)) std::cout<<" x";
+            if (checkVector(index, game.strikes)) std::cout<<" x";
             else if (checkVector(index, game.spares)) std::cout << " /";
             else std::cout<<" "<<game.rolls[index];
         }
     }
-    if(game.scoresPerFrame.size()==10) std::cout<<" | ";
+    if (game.scoresPerFrame.size()==10) std::cout<<" | ";
     std::cout<<"\n ";
 
     // printing scores
     for (auto scoreIt = game.scoresPerFrame.begin(); scoreIt != game.scoresPerFrame.end(); scoreIt++){
         std::cout<<"|";std::cout<<"___";
-        if(*scoreIt>100)std::cout<<*scoreIt;
-        else if(*scoreIt>10) std::cout<<"_"<<*scoreIt;
+        if (*scoreIt>100)std::cout<<*scoreIt;
+        else if (*scoreIt>10) std::cout<<"_"<<*scoreIt;
         else std::cout<<"_"<<*scoreIt<<"_";
         std::cout<<"___";
     }
-    if(game.scoresPerFrame.size()==10)std::cout<<"|";
+    if (game.scoresPerFrame.size()==10)std::cout<<"|";
     std::cout<<"\n\n";
 }
 
 bool Interface::checkVector(int index, std::vector <int> vec) {
-    for(auto  it = vec.begin();it!=vec.end();it++){
+    for (auto  it = vec.begin();it!=vec.end();it++){
         if (index==*it) return true;
     }
     return false;
--- src/BowlingGame_main.cpp
+++ src/BowlingGame_main.cpp
@@ -9,13 +9,11 @@ int main (int argc, char *argv[]) {
     Interface console;                      // Requesting output and displaying score class
     int pins;
     // Player gets 20 rolls unless a spare or strike is rolled in the last frame, when it is 21.
-    while(console.getRollNumber()<21 ||(console.getRollNumber()==21&&game.isExtraRoll())) {
-
+    while (console.getRollNumber() < 21 || (console.getRollNumber() == 21 && game.isExtraRoll())) {
         pins = console.nextRoll(game);
         game.roll(pins);
         console.displayScore(game);
         console.prepareNextRoll(game);
-
     }
     console.getScore(game);
     return 0;
